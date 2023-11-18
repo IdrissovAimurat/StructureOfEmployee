eee8e4752e998e302a652c6e989592cfc52b4464
call me ai|sem <aimur349Gmail.com>
2023-11-19 02:58:57 +0600

    Добавьте файлы проекта.

---
+++ StructureOfEmployee/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructureOfEmployee
+{
+    /// <summary>
+    ///1.	Создать структуру «Employee» состоящую из:
+    ///     a.поля name строкового типа;
+    ///     b.поля vacancy типа Vacancies;
+    ///     c.поля дата приема на работу типа int[3].
+    ///     d.поля зарплата целого типа;
+    /// </summary>
+    
+    public struct Employee
+    {
+        public string Name;
+        public Vacancies Vacancy;
+        public int[] DateOfHire; //int[3]? Типа массив из 3 чисел: День, Месяц и Год? 
+        public int Salary;
+    }
+    
+    public enum Vacancies
+    {
+        Fuhrer,
+        Soldier,
+        Cancellarius,
+        Adviser,
+        //ну и другие...
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
